a864c06be0d08d9d9b78ce310a46d0729f384e19
Poups <62133231+TomPoupi@users.noreply.github.com>
2020-10-14 23:57:57 +0200

    Update main.go
    Du coup j'ai commenté, j'en ai m'y pas mal pour qu'on puisse s'y retrouver facilement après si vous trouvez qu'il en a trop, je vous invite à faire vos proposition ;-)
    J'ai aussi enlever quelque println de vérif  inutile

--- GO/src/hello/main.go
+++ GO/src/hello/main.go
@@ -1,3 +1,7 @@
+// Projet Go grTC1_8:  Dijkstra
+//Tom POUPARD
+//Pierre RENAUD
+//Enzo ZATTARIN 
 package main
 
 import (
@@ -9,7 +13,10 @@ import (
 	"strings"
 	"sync"
 )
-
+//*******************************************************************************
+//
+//*******************************************************************************
+// fonction de l'alogorithm dijkstra appeler par une go routine 
 func dijkstra(sommet string, liens map[string][][]string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
@@ -17,39 +24,40 @@ func dijkstra(sommet string, liens map[string][][]string, wg *sync.WaitGroup) {
 	fmt.Print(sommet)
 	fmt.Println(liens[sommet])*/
 }
-
+//********************************************************************************
+//
+//********************************************************************************
+// Programme principale
 func main() {
 	//hourOfDay := time.Now().Hour()
 	//greeting := getGreeting(hourOfDay)
 	//fmt.Println(greeting)
-	file, err := os.Open("schema.txt")
+	file, err := os.Open("schema.txt") // permet d'ouvir le fichier txt en appelant file
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	defer file.Close()// ferme le fichier quand le main est fini 
 
 	compteurLiens := 0
 
 	scanner := bufio.NewScanner(file)
 
-	scanner.Scan() //On lit la première ligne du doc pour avoir le nombre de liens
-	//fmt.Println(scanner.Text())
+	scanner.Scan() //On lit la première ligne du doc pour avoir le nombre de liens avec .scan()
+	fmt.Println(scanner.Text())
 
-	n, err := strconv.Atoi(scanner.Text())
+	n, err := strconv.Atoi(scanner.Text()) //On le convertie en entier pour pouvoir creer nos slice de slice : "tableau de liens"
 	if err != nil {
 		// handle error
 		fmt.Println(err)
 		os.Exit(2)
 	}
-	liens := make([][]string, n)
-	//fmt.Println(liens)
-
-	for scanner.Scan() { //On remplit un slice avec les différents liens
-		//fmt.Println(scanner.Text())
-
-		lien := strings.Split(scanner.Text(), " ")
-		//fmt.Println(lien)
-		liens[compteurLiens] = lien
+	liens := make([][]string, n) // notre slice de slice
+	
+	//On remplit un slice avec les différents liens
+	for scanner.Scan() { // on parcours tous le fichier ligne par ligne  
+		
+		lien := strings.Split(scanner.Text(), " ")// on lit chaque ligne avec la fonction scanner.text() et on créer un slice de string avec la fonction strings.Split avec comme indication " " comme séparateur  
+		liens[compteurLiens] = lien // on associe pour chaque item de la "slice de slice" "un lien"(sous forme d'un slice) du fichier texte dans l'ordre chronologique 
 		compteurLiens++
 
 	}
@@ -58,21 +66,28 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	m := make(map[string][][]string)
-	for _, lien := range liens { //On remplit la map
-
-		if m[lien[0]] == nil { //On ajoute le sommet si inexistant
-			m[lien[0]] = make([][]string, 0)
+	
+	m := make(map[string][][]string)// création d'une map
+	//On remplit la map avec les élément de notre " slice de slice "  
+	for _, lien := range liens { // on parcours chaque "lien" de "liens" (slice de slice) 
+		//Idée : On parcours tout liens, dans chaque lien on lit les 2 premiers item qui sont les sommets
+		//Ensuite on teste s'ils ont déja été créé dans dans la map si non on les crées, ces sommets seront les clés de la map   
+		if m[lien[0]] == nil { 
+			m[lien[0]] = make([][]string, 0)// on associe une clé à un tableau 
 		}
-		if m[lien[1]] == nil {
+		if m[lien[1]] == nil { 
 			m[lien[1]] = make([][]string, 0)
 		}
-		m[lien[0]] = append(m[lien[0]], lien)
+		//Enfin on rempli chaque tableau de la clé avec le lien associé(càd l'iteration en cours)  
+		m[lien[0]] = append(m[lien[0]], lien)// append permet de pouvoir aggrandir la taille de ce tableau en lui mettant un lien   
 		m[lien[1]] = append(m[lien[1]], lien)
+		// exemple : le tableau de "B" va être creer à l'itération 1, ensuite il va être rempli avec le lien 1 ET à l'itération 2 et 3 on va l'agrendir en lui mettant le lien 2 et le lien 3
+		// B :[[lien1][lien2][lien3]]
 	}
 
+	// Utilisation du principe de go routine en executant simultanément plusieur fonction tout en evitant que l'execution des fonction se chevauche
 	var wg sync.WaitGroup
+	//fmt.Println(m)
 
 	for sommet, _ := range m {
 		wg.Add(1)
@@ -81,24 +96,12 @@ func main() {
 	}
 	wg.Wait()
 
-	//go routine
-	//entree : sommet du graphe
-	//sortie : les chemins les plus courts vers les autres sommets
-
-	//on lance une routine par sommet dans m
-
-	//nbLiens=
-
-	//lien3 :=
-	//m:=make(map[string][]string)
-
-	//lien1
-	//if lien1[0] == [] {
-	//m[lien1[0]]=[1][]string{lien1}
-	//}
-
-	//m["A"] = [lien1]
-	//m["B"] = [lien1, lien2]
-	//m["C"] = [lien2]
-
 }
+//***********************************************************************************
+//
+//***********************************************************************************
+//	Objectif
+//go routine
+//entree : sommet du graphe
+//sortie : les chemins les plus courts vers les autres sommets
+//on lance une routine par sommet dans m
